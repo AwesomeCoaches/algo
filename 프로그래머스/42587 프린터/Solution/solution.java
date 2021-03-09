649eb299155eb3f6879dc37976ce53365fba7ac7
KimNackHyeon <33438066+KimNackHyeon@users.noreply.github.com>
2021-03-09 11:51:44 +0900

    Create solution.java

---
+++ 프로그래머스/42587 프린터/Solution/solution.java
@@ -0,0 +1,31 @@
+public static int solution(int[] priorities, int location) {
+
+		int[] prints = new int[priorities.length];
+
+		List<int[]> list = new ArrayList<>();
+		for (int i = 0; i < priorities.length; i++) {
+			list.add(new int[] { i + 1, priorities[i] });
+		}
+
+		int priority = 0;
+		while (!list.isEmpty()) {
+			int index = 0;
+			boolean flag = false;
+			for (int j = index + 1; j < list.size(); j++) {
+				if (list.get(index)[1] < list.get(j)[1]) { // 우선순위 비교 중 우선순위가 높은 것이 있다면
+					int[] temp = list.remove(index); // 순서를 맨 뒤로
+					list.add(temp);
+					flag = true;
+					break;
+				}
+			}
+
+			if (!flag) {
+				prints[list.remove(index)[0] - 1] = priority + 1;
+				priority++;
+			}
+			index++;
+		}
+
+		return prints[location];
+	}
